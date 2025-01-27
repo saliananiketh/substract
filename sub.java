6cfd46a76416abb481df9a769463b3a24d653091
saliananiketh <anikethhh69@gmail.com>
2025-01-27 15:29:52 +0530

    Update sub.java

--- sub.java
+++ sub.java
@@ -1,11 +1,11 @@
 class sub
 {
- public static void main(String args[])
+  public static void main(String args[])
   {
     int a,b,sub;
     a=9;
     b=4;
     sub=a-b;
-    System.out.println("substraction of 2 numbers is",+sub);
+    System.out.println("substraction of 2 numbers is:" +sub);
   }
 }
